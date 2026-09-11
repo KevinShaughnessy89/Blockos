d649e38007d5b5f3ab735fad9cdc7de77e18a742
gurijb-ftk <gurijb2016@gmail.com>
2026-09-11 18:46:07 +0200

    Add get_mac_address function to virtio_net_driver
    Added function to retrieve MAC address.

--- drivers/virtio_net_driver.hpp
+++ drivers/virtio_net_driver.hpp
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace virtio_net {
-    bool init();
-    bool is_available();
-    bool send_packet(const void* data, unsigned len);
-    int receive_packet(void* buf, unsigned buf_len);
-    // Reclaim completed TX descriptors and recycle buffers
-    void reclaim_tx();
+
+bool init();
+bool is_available();
+
+bool send_packet(
+    const void* data,
+    unsigned len
+);
+
+int receive_packet(
+    void* buf,
+    unsigned buf_len
+);
+
+void reclaim_tx();
+
+// ------------------------------------------------------------
+// MAC address
+// ------------------------------------------------------------
+
+bool get_mac_address(
+    uint8_t out_mac[6]
+);
+
 }
