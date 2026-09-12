d418235d2b2e16b58644b0cb98926c15540a619f
gurijb-ftk <gurijb2016@gmail.com>
2026-09-12 16:36:54 +0200

    Refactor Xinerama header for better formatting
    Reformatted XineramaScreenInfo struct and function declarations for improved readability.

--- ports/bx11/include/X11/extensions/Xinerama.h
+++ ports/bx11/include/X11/extensions/Xinerama.h
@@ -1,12 +1,39 @@
 #pragma once
+
 #include "../Xlib.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-typedef struct { int screen_number; short x_org,y_org; unsigned short width,height; } XineramaScreenInfo;
-Bool XineramaQueryExtension(Display*, int*, int*);
-Bool XineramaIsActive(Display*, Window);
-XineramaScreenInfo *XineramaQueryScreens(Display*, int*);
+
+typedef struct
+{
+    int screen_number;
+
+    short x_org;
+    short y_org;
+
+    unsigned short width;
+    unsigned short height;
+
+} XineramaScreenInfo;
+
+Bool XineramaQueryExtension(
+    Display*,
+    int*,
+    int*
+);
+
+Bool XineramaIsActive(
+    Display*,
+    Window
+);
+
+XineramaScreenInfo* XineramaQueryScreens(
+    Display*,
+    int*
+);
+
 #ifdef __cplusplus
 }
 #endif
